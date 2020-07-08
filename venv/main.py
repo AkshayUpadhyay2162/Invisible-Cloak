645f66ff55acccbc0c3c19991fc21b72bb40c407
Akshay Upadhyay <akshayu383@gmail.com>
2020-07-08 14:36:47 +0530

    New update

--- venv/main.py
+++ venv/main.py
@@ -1,9 +1,20 @@
 # Harry potter theme invisible cloak project.
-# import libraries
+# Akshay JS Upadhyay
 
+# import libraries
 import numpy as np
 import cv2
 
 import time
 
-cap = cv2.VideoCapture("")
+cap = cv2.VideoCapture(0)           # to select the webcam.
+time.sleep(2)
+
+background = 0
+
+# capturing the background
+for i in range(30):
+    ret, background = cap.read()
+
+while(cap.isOpened()):
+    ret, img = cap.read()
